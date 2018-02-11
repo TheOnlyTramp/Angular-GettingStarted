92ce1c6b92cc5bd9d7e44ded628c91f029d882ae
Gareth MacCauley <gmaccauley@gmail.com>
2018-02-11 09:20:53 +0000

    Updates

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
+
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products: any[] = [
+  products: IProduct[] = [
     {
       "productId": 2,
       "productName": "Garden Cart",
@@ -33,6 +36,10 @@ export class ProductListComponent{
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
   }
   ];
+
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
 
   toggleImage(): void{
     this.showImage= !this.showImage;
